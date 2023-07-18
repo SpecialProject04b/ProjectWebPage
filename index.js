7b630f4c206df71ab7374c0fb3e5615c1a465512
alexNeoKs <neoalexanderks@gmail.com>
2023-07-18 21:24:28 +0800

    Added nodejs

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+
+
+
+
